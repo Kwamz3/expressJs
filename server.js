f87eb1bec068e8c0f1a209067a256d23ea9a1cc1
Kwamz3 <charlie18appiah@gmail.com>
2026-05-19 15:14:19 +0000

    feat: add homepage and dashboard routes and update server listening log format

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// *** Website endpoints ***
+
+app.get('/', (req, res) => {
+    res.send('<h1>homepage</h1>');
+})
+
+app.get('/dashboard', (req, res) => {
+    res.send('<h1>dashboard</h1>');
+})
+
+// *** API endpoints ***
+
+
+
 app.listen(PORT, () => {
-    console.log(`Server is running on: ${PORT}`);
+    console.log(`Server is running on http://localhost:3000`);
 })
