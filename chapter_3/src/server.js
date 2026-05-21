9e119d42154e3fd725e4430ffbf3a99f4d78ce8a
Kwamz3 <charlie18appiah@gmail.com>
2026-05-21 11:38:37 +0000

    feat: set up chapter 3 with server, styles, and initial HTML structure

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,1 +1,10 @@
-    
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+console.log('Hello world')
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:3000`);
+})
