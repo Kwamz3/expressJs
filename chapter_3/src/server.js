2c4deaa8bd07c2256535742becde83d02940aba0
Kwamz3 <charlie18appiah@gmail.com>
2026-05-22 14:48:58 +0000

    feat: add initial route for homepage in server.js

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// getting the file path from the current module
+const __filename = fileURLToPath(import.meta.url)
+
+// getting the directory name from the file path
+const __dirname = dirname(__filename)
+
+app.get('/', (req, res) => {
+    res.sendFile(path)
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:3000`);
