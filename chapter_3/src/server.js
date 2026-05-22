40e7ae8fb9dd0430c440749d7f41e2ce09f03a96
Kwamz3 <charlie18appiah@gmail.com>
2026-05-22 14:31:01 +0000

    chore: remove unnecessary console log from server.js

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -3,8 +3,6 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT || 3000
 
-console.log('Hello world')
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:3000`);
 })
